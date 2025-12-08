38cfb09b19d750ced2202b6feabde953e6ff066a
Diego Santana <102614023+diegoednaldo@users.noreply.github.com>
2025-12-08 11:11:52 -0300

    Add SELECT statements for cursos and categorias

--- 3_operacoes.sql
+++ 3_operacoes.sql
@@ -1,13 +1,17 @@
 USE db_cursos_online;
 
--- DEMONSTRAÇÃO DML: UPDATE
+SELECT * FROM cursos;
+
 -- Requisito: Exemplo de UPDATE (Mudar o preço do curso de Python)
 UPDATE cursos
 SET preco = 219.90, carga_horaria_horas = 45
 WHERE id_curso = 4;
+SELECT * FROM cursos;
 
--- 10. DEMONSTRAÇÃO DML: DELETE
--- Requisito: Exemplo de DELETE (Remover uma categoria vazia)
 INSERT INTO categorias (nome, descricao) VALUES ('Gastronomia', 'Cursos de culinária e alimentos.');
+SELECT * FROM categorias;
+
+-- Requisito: Exemplo de DELETE (Remover uma categoria vazia)
 DELETE FROM categorias
 WHERE nome = 'Gastronomia';
+SELECT * FROM categorias;
